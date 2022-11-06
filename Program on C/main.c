9ed680a6961f6c89935cc925989bc514e27413fc
Bishop-Y <66753948+Bishop-Y@users.noreply.github.com>
2022-11-06 21:24:24 +0300

    Update main.c

--- Program on C/main.c
+++ Program on C/main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <time.h>
 
-int fillStringManual(char array[100000]);
-int fillStringFile(char array[100000], const char *inputFile);
-int generation(char array[100000]);
-int function(const char array[100000], int buffer);
-void writeStringFile(int result, const char *outputFile);
+int function(const char array[200], int size);
 
-int main(int argc, char *argv[])
-{
-    char string[100000];
-    int result, buffer;
+int main(int argc, char *argv[]) {
+    char string[200];
+    int size = 0;
+    int ch;
+    int result;
 
-    if (argc == 3)
-    {
-        buffer = fillStringFile(string, argv[1]);
-        result = function(string, buffer);
-        writeStringFile(result, argv[2]);
+    if (argc == 3) {
+        FILE *input;
+        input = fopen(argv[1], "r");
+        if (input == NULL) {
+            printf("File does not exist");
+            return 0;
+        } else {
+            do {
+                ch = fgetc(input);
+                string[size++] = ch;
+            } while (ch != -1 && size < 200);
+            string[size - 1] = '\0';
+        }
+        fclose(input);
+
+        result = function(string, size);
+
+        FILE *output;
+        output = fopen(argv[2], "w");
+        if (result) {
+            fprintf(output, "The nested sequence of parentheses is correct.\n");
+        } else {
+            fprintf(output, "The nested sequence of parentheses is incorrect.\n");
+        }
+        fclose(output);
         return 0;
     }
 
@@ -25,32 +42,44 @@ int main(int argc, char *argv[])
     int inputType;
     scanf("%d", &inputType);
 
-    if (inputType == 1)
-    {
-        buffer = fillStringManual(string);
-    }
-    else
-    {
-        buffer = generation(string);
-    }
-
-    time_t start_time = clock();
-    int i;
-    for (i = 0; i < 1000000; ++i)
-    {
-        result = function(string, buffer);
-    }
-    time_t end_time = clock();
-
-    if (result)
-    {
-        printf("\nThe nested sequence of parentheses is correct.");
-    }
-    else
-    {
-        printf("\nThe nested sequence of parentheses is incorrect.");
+    int index = 0;
+    if (inputType == 1) {
+        printf("\nType your string: ");
+        do {
+            ch = fgetc(stdin);
+            string[size++] = ch;
+        } while (ch != -1 && size < 200);
+        string[size - 1] = '\0';
+    } else {
+        int seed;
+        printf("\nEnter seed: ");
+        scanf("%d", &seed);
+        srand(seed);
+        size = rand() % 200;
+        printf("\n");
+        do {
+            string[index] = 33 + rand() % 95;
+            printf("%c", string[index]);
+            ++index;
+        } while (index < size);
     }
 
-    printf("\nTime: %d\n", (int)difftime(end_time, start_time) / 1000);
+    if (argc == 2) {
+        time_t start_time = clock();
+        for (index = 0; index < 20000000; ++index) {
+            result = function(string, size);
+        }
+        time_t end_time = clock();
+        printf("\nTime: %d\n", (int) difftime(end_time, start_time) / 1000);
+    } else {
+        result = function(string, size);
+    }
+
+    if (result) {
+        printf("\n\nThe nested sequence of parentheses is correct\n.");
+    } else {
+        printf("\n\nThe nested sequence of parentheses is incorrect\n.");
+    }
+
     return 0;
 }
